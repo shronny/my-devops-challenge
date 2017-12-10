e7d91a34614bd9e6b712dae589bb52e9b6551115
Ronny Sharaby <sh.ronny@gmail.com>
2017-12-10 21:42:57 +0200

    commit fot travis testing
    remark @app.js

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-//var index = require('./routes/index');
+//var index = require('./routes/index'); // Disable root route
 var secret = require('./routes/secret');
 
 var app = express();
